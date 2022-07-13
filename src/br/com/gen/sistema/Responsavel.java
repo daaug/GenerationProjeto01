cc3e86eb696f9cd0b72b4e94311f76e97b211867
Rafael Silveira <rafaell.ssouza@hotmail.com>
2022-07-13 17:38:03 -0300

    Adicionado na classe responsavel as variaveis 'ra' e 'nome', getters e setters dos mesmos

--- src/br/com/gen/sistema/Responsavel.java
+++ src/br/com/gen/sistema/Responsavel.java
@@ -2,4 +2,23 @@ package br.com.gen.sistema;
 
 public class Responsavel {
 
+	private int ra;
+	private String nome;
+
+	public int getRA() {
+		return ra;
+	}
+
+	public void setRA(int ra) {
+		this.ra = ra;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
